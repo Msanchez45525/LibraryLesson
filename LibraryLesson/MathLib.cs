2a297299eccdd050c5460a13ddc50816efab4669
Matt Sanchez <msanchez@tql.com>
2021-05-25 12:13:46 -0400

    Add project files.

---
+++ LibraryLesson/MathLib.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LibraryLesson
+{
+    public class MathLib
+    {
+        private static int b;
+        private static int a;
+
+        // subtract multiply devide modulo
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+
+        public static int Modulo(int a, int b)
+            {
+            return Sub(a,Mult(Div(a, b), b));
+
+            
+        }
+
+        // modulo = Mod(a,b) = a - ((a/b)*b)
+        // Mod(5,3) = 5 - ((5/3)*c)
+    } //return Sub(a, Mult(Div(a, b),b));
+}
---
+++ TestLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using LibraryLesson;
+using System;
+
+namespace TestLibrary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var OnePlusTwo = MathLib.Add(1, 2);
+
+            var SubTenV = MathLib.Sub(10, 5);
+
+            var TenFive = MathLib.Mult(10, 5);
+
+            var TenOne = MathLib.Div(10, 1);
+
+            var TenTwo = MathLib.Modulo(10, 2);
+        }
+        
+        
+
+    }
+
+}
